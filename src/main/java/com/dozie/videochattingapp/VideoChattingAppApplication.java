156112b2e0a8d50980f0cb1c2843d9621cec9021
Chukwuebuka Uyanwune <dozie@golivwell.com>
2024-11-03 17:49:08 +0100

    first commit

---
+++ src/main/java/com/dozie/videochattingapp/VideoChattingAppApplication.java
@@ -0,0 +1,13 @@
+package com.dozie.videochattingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoChattingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoChattingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dozie/videochattingapp/VideoChattingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dozie.videochattingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoChattingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
