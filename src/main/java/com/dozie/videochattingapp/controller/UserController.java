cc2f31b888d90c6418dd73e3a6974338e5cdc64f
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2024-11-04 06:16:17 +0100

    completed user controller

--- src/main/java/com/dozie/videochattingapp/controller/UserController.java
+++ src/main/java/com/dozie/videochattingapp/controller/UserController.java
@@ -1,2 +1,31 @@
-package com.dozie.videochattingapp.controller;public class UserController {
+package com.dozie.videochattingapp.controller;
+
+import com.dozie.videochattingapp.service.UserService;
+import com.dozie.videochattingapp.user.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@Slf4j
+public class UserController {
+
+    private final UserService userService;
+
+    public void registerUser(User user){
+        userService.registerUser(user);
+    }
+
+    public User login(User user){
+        return userService.login(user);
+    }
+
+    public void logout(String email){
+        userService.logout(email);
+    }
 }
